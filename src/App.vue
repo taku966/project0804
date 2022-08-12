f05cc59faee79b793461fac55cde16a77d1fede9
taku <taku20030819gmail.com>
2022-08-12 10:34:21 +0900

    ✨ EventsViewのRouterの設定

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
+    |
     <router-link to="/about">About</router-link>
+    |
+    <router-link to="/events">Events</router-link>
   </nav>
   <router-view/>
 </template>
